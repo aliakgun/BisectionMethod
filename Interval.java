994cd0f2e167e85bbb264a2132653248f406d4f0
Ali Akgün <47610071+aliakgun@users.noreply.github.com>
2019-07-29 00:34:48 +0300

    Add files via upload

---
+++ Interval.java
@@ -0,0 +1,23 @@
+
+public class Interval {
+	
+	double initial_value; /// Initial value of the root interval.
+	double final_value; /// Final value of the root interval.
+	double mid_value;
+	
+	static double calculate(double point) {
+		return point * point - 9;
+	}
+	
+	Interval(double new_initial_value, double new_final_value) {
+		new_initial_value = initial_value;
+		new_final_value = final_value;
+		
+		while(true) {
+			if (calculate(new_initial_value) * calculate(new_final_value) < 0) {
+				new_final_value = (new_initial_value + new_final_value) / 2;
+			}
+		}
+	}
+	
+}
