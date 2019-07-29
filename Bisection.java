a530ec2216ac2d1debe4d188b785a20f9da170cc
Ali Akgün <47610071+aliakgun@users.noreply.github.com>
2019-07-30 00:06:44 +0300

    Update Bisection.java

--- Bisection.java
+++ Bisection.java
@@ -1,21 +1,35 @@
-
-public class Bisection {
+/**
+ * 
+ * @author Ali akgun
+ * @todo develop algorithm !! :)
+ * @brief
+ * @date 30.07.2019
+ * @bug
+ * initial value, final value , and mid value data type problem
+ * when they are int this program work successfully.
+ */
+public class Interval {
 	
 	double initial_value; /// Initial value of the root interval.
 	double final_value; /// Final value of the root interval.
 	double mid_value;
+	double root; //root of the function.
 	
 	static double calculate(double point) {
 		return point * point - 9;
 	}
 	
-	Interval(double new_initial_value, double new_final_value) {
-		new_initial_value = initial_value;
-		new_final_value = final_value;
+	public Interval(double new_initial_value, double new_final_value) {
+		initial_value = new_initial_value;
+		final_value = new_final_value;
 		
 		while(true) {
-			if (calculate(new_initial_value) * calculate(new_final_value) < 0) {
-				new_final_value = (new_initial_value + new_final_value) / 2;
+			if ((calculate(initial_value) * calculate(final_value)) < 0) {
+				initial_value = (initial_value + final_value) / 2.0;
+			
+			}else if (calculate(initial_value) <= 0.1) {
+				root = initial_value;
+				break;
 			}
 		}
 	}
